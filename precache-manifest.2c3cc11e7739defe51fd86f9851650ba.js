9d0b026bc6e1d292ebe29df505ab52bde64d63ab
redant23 <kimdongjun23@gmail.com>
2020-03-08 02:21:03 +0900

    Updates

---
+++ precache-manifest.2c3cc11e7739defe51fd86f9851650ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ec501e1164eae250b1f51551f6b988",
+    "url": "/23-52/index.html"
+  },
+  {
+    "revision": "7dec40b4a3b2dc024dd2",
+    "url": "/23-52/static/css/main.48ee04ef.chunk.css"
+  },
+  {
+    "revision": "dfcef11e6fc77a62372d",
+    "url": "/23-52/static/js/2.706fdd38.chunk.js"
+  },
+  {
+    "revision": "5c612f3dd92743aaf775dfcd58ab2706",
+    "url": "/23-52/static/js/2.706fdd38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dec40b4a3b2dc024dd2",
+    "url": "/23-52/static/js/main.339b4fea.chunk.js"
+  },
+  {
+    "revision": "67ea4f5e3a8283fd9342",
+    "url": "/23-52/static/js/runtime-main.bb40ea49.js"
+  }
+]);
